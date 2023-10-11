c3ade9d7f86eb24a2c1029bfebeb7a1e98698310
Manisha <manprajapati268@gmail.com>
2023-10-11 23:43:21 +0530

    started project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello Game Sky</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/api.jsx
@@ -0,0 +1,7 @@
+// Base URL
+
+const base_url = "https://api.rawg.io/api/";
+
+// Popular games
+
+const popular_games = "https://api.rawg.io/api/games?dates=2023";
